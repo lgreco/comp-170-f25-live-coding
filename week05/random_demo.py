8c913262a7a1d1af447dd91873a71cdc8400831d
Leo Irakliotis <lgreco@gmail.com>
2025-10-03 13:44:54 +0000

    updated random

--- week05/random_demo.py
+++ week05/random_demo.py
@@ -1,20 +1,35 @@
-
 import random
 
-correct_guess = False
-attempt = 0
+RANGE_FROM = 0
+RANGE_TO = 10
+MAX_ATTEMPTS = 3
 
-number_to_guess = random.randint(0,10)
 
-while not correct_guess and attempt < 3:
-    n = input("\nGuess which number I am thinking of: ")
-    n = int(n)
-    attempt = attempt + 1
-    correct_guess = (n == number_to_guess)
-    if not correct_guess:
-        print("Nope!")
+def guessing_game(
+    range_from: int = RANGE_FROM,
+    range_to: int = RANGE_TO,
+    max_attempts: int = MAX_ATTEMPTS,
+):
 
-if correct_guess:
-    print(f"\nCongratulations. {n} is indeed the number I was thinking off.")
-else:
-    print(f"\nBummer, it was {number_to_guess}. Better luck next time, LOOSER!")
+    correct_guess = False
+    attempt = 0
+
+    number_to_guess = random.randint(range_from, range_to)
+
+    while not correct_guess and attempt < max_attempts:
+        n = input("\nGuess which number I am thinking of: ")
+        n = int(n)
+        attempt = attempt + 1
+        correct_guess = n == number_to_guess
+        if not correct_guess:
+            print("Nope!")
+
+    if correct_guess:
+        print(f"\nCongratulations. {n} is indeed the number I was thinking off.")
+    else:
+        print(f"\nBummer, it was {number_to_guess}. Better luck next time, LOOSER!")
+
+def main():
+    guessing_game()
+
+main()
