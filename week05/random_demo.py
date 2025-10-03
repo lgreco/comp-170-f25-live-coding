bc0d7bc967350b254d69709cba1d27ee972febda
Leo Irakliotis <lgreco@gmail.com>
2025-10-03 13:57:09 +0000

    ANSI codes for screen clearing

--- week05/random_demo.py
+++ week05/random_demo.py
@@ -10,20 +10,26 @@ def guessing_game(
     range_to: int = RANGE_TO,
     max_attempts: int = MAX_ATTEMPTS,
 ):
-
+    """A simple number guessing game."""
+    # Initialize the game state
     correct_guess = False
     attempt = 0
-
     number_to_guess = random.randint(range_from, range_to)
-
+    # Start the game by informing the user of the rules
+    print("\033[2J\033[H", end="")  # Clear the screen using ANSI escape codes
+    print(f"I'm thinking of a number between {range_from} and {range_to}.")
+    print(f"You have {max_attempts} attempts to guess it.")
+    # Loop until the user guesses correctly or runs out of attempts
     while not correct_guess and attempt < max_attempts:
         n = input("\nGuess which number I am thinking of: ")
+        # Clean the input
         n = int(n)
-        attempt = attempt + 1
+        # Update the game state
+        attempt = attempt + 1 # attempt += 1
         correct_guess = n == number_to_guess
         if not correct_guess:
             print("Nope!")
-
+    # Game over, print the result
     if correct_guess:
         print(f"\nCongratulations. {n} is indeed the number I was thinking off.")
     else:
